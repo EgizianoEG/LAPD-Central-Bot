0ee2e3baccd45e889ed8874e59a466851a16ce3a
Egiziano <EgizianoEG@users.noreply.github.com>
2023-08-02 04:39:34 +0300

    Test

--- Source/Main.js
+++ Source/Main.js
@@ -2,7 +2,6 @@ const { Client, Collection, GatewayIntentBits, IntentsBitField } = require("disc
 const {
   Discord: { BotToken },
   Roblox: { Cookie },
-  OpenWeather,
 } = require("./Json/Secrets.json");
 
 const Chalk = require("chalk");
