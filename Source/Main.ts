3ef975367d471cbb79d688aea1044033b1fb7ae1
EgizianoEG <56704274+EgizianoEG@users.noreply.github.com>
2023-11-01 02:12:12 +0200

    Update Main.ts

--- Source/Main.ts
+++ Source/Main.ts
@@ -1,41 +1,35 @@
-import { Discord as DiscordSecrets, Roblox as RobloxSecrets } from "@Config/Secrets.js";
 import { Client, Collection, GatewayIntentBits } from "discord.js";
+import { Discord as DiscordSecrets } from "@Config/Secrets.js";
+import { GetDirName } from "@Utilities/Other/Paths.js";
 
+import Path from "node:path";
 import Chalk from "chalk";
-import Noblox from "noblox.js";
-import EventHandler from "./Handlers/EventHandler.js";
-import MongoDBHandler from "./Handlers/MongoDB.js";
-
+import GetFiles from "@Utilities/Other/GetFilesFrom.js";
 console.log(Chalk.grey("================================================"));
 // ----------------------------------------------------------------------------------
 
 const App = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-  ],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages],
 });
 
 App.commands = new Collection();
 App.cooldowns = new Collection();
-
 App.login(DiscordSecrets.BotToken)
   .then(() => {
     if (!App.user) throw new Error("`App.user` is not accessible.");
     console.info("✅ - %s bot is online.", Chalk.cyanBright.bold(App.user.username));
   })
-  .catch((Err) =>
-    console.error("❎ - %s", Chalk.red("Failed to run the application. Details:\n"), Err)
-  );
+  .catch((Err) => console.error("❎ - %s", Chalk.red("Failed to run the application. Details:\n"), Err));
 
-Noblox.setCookie(RobloxSecrets.Cookie)
-  .then((UserData) => {
-    console.log("✅ - Logged into Roblox as %s.", Chalk.cyanBright.bold(UserData.UserName));
-  })
-  .catch((Err) => {
-    console.log("❎ - %s Could not log into Roblox. Details:\n", Chalk.red("ERROR"), Err);
-  });
+(async function RunHandlers() {
+  const DirPath = Path.join(GetDirName(import.meta.url), "Handlers");
+  const Files = GetFiles(DirPath);
 
-EventHandler(App);
-MongoDBHandler();
+  for (const File of Files) {
+    import(File).then((Module) => {
+      if (typeof Module.default === "function") {
+        Module.default(App);
+      }
+    });
+  }
+})();
