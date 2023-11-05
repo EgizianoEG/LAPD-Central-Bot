a81b0929123dc2a0f44e930e3ddafe408641074b
EgizianoEG <56704274+EgizianoEG@users.noreply.github.com>
2023-11-06 00:03:03 +0200

    Update EventHandler.ts
    Fix the way the event name is being obtained, validate events that are being listened to, remove the debugging `console.log` statements, and add additional logging details.

--- Source/Handlers/EventHandler.ts
+++ Source/Handlers/EventHandler.ts
@@ -1,16 +1,18 @@
 import { GetDirName } from "@Utilities/Other/Paths.js";
 import { CamelCase } from "@Utilities/Strings/Converter.js";
+import { Events } from "discord.js";
 import AppLogger from "@Utilities/Classes/AppLogger.js";
 import GetFiles from "@Utilities/Other/GetFilesFrom.js";
 import Path from "node:path";
 
 export default async function EventHandler(Client: DiscordClient) {
   const EventFolders = GetFiles(Path.join(GetDirName(import.meta.url), "..", "Events"), true);
-  console.log("RenderTesting - EventHandler; using event folders: %o", EventFolders);
+  const ListenersData: Record<string, number> = {};
+  const ClientEvents = Object.values(Events) as string[];
 
   for (const EventFolder of EventFolders) {
     const FuncsToExecute: ((Client: DiscordClient, ...Args: any[]) => any)[] = [];
-    const EventName = CamelCase(EventFolder.match(/[^\\]+$/)?.[0] ?? "");
+    const EventName = CamelCase(Path.basename(EventFolder) ?? "");
     const EventFiles = GetFiles(EventFolder).sort((a, b) => {
       const a_pos = a.match(/\[(\d+)\]/)?.[1];
       const b_pos = b.match(/\[(\d+)\]/)?.[1];
@@ -21,12 +23,6 @@ export default async function EventHandler(Client: DiscordClient) {
       else return 0;
     });
 
-    console.log(
-      "RenderTesting - EventHandler; processing event folder '%s' with %o total files",
-      Path.basename(EventFolder),
-      EventFiles.length
-    );
-
     for (const EventFile of EventFiles) {
       const EventFunc = (await import(EventFile)).default as (typeof FuncsToExecute)[number];
       if (typeof EventFunc === "function") {
@@ -34,19 +30,29 @@ export default async function EventHandler(Client: DiscordClient) {
       }
     }
 
-    console.log(
-      "RenderTesting - EventHandler; listening for event '%s' with %o total functions",
-      EventName,
-      FuncsToExecute.length
-    );
-
-    Client.on(EventName, (...Args) => {
-      FuncsToExecute.forEach((Func) => Func(Client, ...Args));
-    });
+    if (ClientEvents.includes(EventName)) {
+      ListenersData[EventName] = FuncsToExecute.length;
+      Client.on(EventName, (...Args) => {
+        FuncsToExecute.forEach((Func) => Func(Client, ...Args));
+      });
+    } else {
+      AppLogger.error({
+        label: "Handlers:EventHandler",
+        message:
+          "Listening to event '%s' was skipped as it does not exist in DiscordJS Client instance.",
+        splat: [EventName],
+        details: {
+          event: EventName,
+          total_funcs: FuncsToExecute.length,
+        },
+      });
+    }
   }
 
   AppLogger.info({
     label: "Handlers:EventHandler",
-    message: "Successfully loaded all event listeners.",
+    message:
+      "Successfully loaded all event listeners. Listening for the following events with total listeners of each: %o",
+    splat: [ListenersData],
   });
 }
