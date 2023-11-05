d8f7353acb47ee36553e972273e7cb32cc5dd0da
EgizianoEG <56704274+EgizianoEG@users.noreply.github.com>
2023-11-05 23:04:26 +0200

    Temporary testing for render deplyment

--- Source/Handlers/EventHandler.ts
+++ Source/Handlers/EventHandler.ts
@@ -6,6 +6,7 @@ import Path from "node:path";
 
 export default async function EventHandler(Client: DiscordClient) {
   const EventFolders = GetFiles(Path.join(GetDirName(import.meta.url), "..", "Events"), true);
+  console.log("RenderTesting - EventHandler; using event folders: %o", EventFolders);
 
   for (const EventFolder of EventFolders) {
     const FuncsToExecute: ((Client: DiscordClient, ...Args: any[]) => any)[] = [];
@@ -20,12 +21,24 @@ export default async function EventHandler(Client: DiscordClient) {
       else return 0;
     });
 
+    console.log(
+      "RenderTesting - EventHandler; processing event folder '%s' with %o total files",
+      Path.basename(EventFolder),
+      EventFiles.length
+    );
+
     for (const EventFile of EventFiles) {
       const EventFunc = (await import(EventFile)).default as (typeof FuncsToExecute)[number];
       if (typeof EventFunc === "function") {
         FuncsToExecute.push(EventFunc);
       }
     }
+
+    console.log(
+      "RenderTesting - EventHandler; listening for event '%s' with %o total functions",
+      EventName,
+      FuncsToExecute.length
+    );
 
     Client.on(EventName, (...Args) => {
       FuncsToExecute.forEach((Func) => Func(Client, ...Args));
@@ -34,6 +47,6 @@ export default async function EventHandler(Client: DiscordClient) {
 
   AppLogger.info({
     label: "Handlers:EventHandler",
-    message: "Successfully Loaded all event listeners.",
+    message: "Successfully loaded all event listeners.",
   });
 }
