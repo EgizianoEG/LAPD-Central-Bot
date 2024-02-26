abd726d880e10defecb6e753855e1289133d78c0
Egiziano <56704274+EgizianoEG@users.noreply.github.com>
2024-02-26 02:03:25 +0200

    random command

---
+++ Source/Commands/Development/RandomShift.ts
@@ -0,0 +1,43 @@
+import ShiftModel from "@Models/Shift.js";
+import { SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js";
+import Dedent from "dedent";
+
+// ---------------------------------------------------------------------------------------
+/**
+ * @param Client
+ * @param Interaction
+ */
+async function Callback(Interaction: SlashCommandInteraction) {
+  const Shift = await ShiftModel.countDocuments({ guild: Interaction.guildId }).then((count) => {
+    const ran_num = Math.floor(Math.random() * count);
+    return ShiftModel.findOne().skip(ran_num).exec();
+  });
+
+  if (Shift) {
+    return Interaction.reply({
+      ephemeral: true,
+      content: Dedent`
+        \`\`\`json
+        ${JSON.stringify(Shift.toJSON({ getters: true }), null, 2)}
+        \`\`\`
+      `,
+    });
+  } else {
+    return Interaction.reply({ ephemeral: true, content: "There are no recorded shifts." });
+  }
+}
+
+// ---------------------------------------------------------------------------------------
+// Command structure:
+// ------------------
+const CommandObject: SlashCommandObject<SlashCommandSubcommandsOnlyBuilder> = {
+  callback: Callback,
+  options: { dev_only: true },
+  data: new SlashCommandBuilder()
+    .setName("random-shift")
+    .setDMPermission(false)
+    .setDescription("Shows a random previously recorded shift."),
+};
+
+// ---------------------------------------------------------------------------------------
+export default CommandObject;
