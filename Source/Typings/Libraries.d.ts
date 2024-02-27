5ddc0eada94d01996b96900407bfec69b69d4062
Egiziano <56704274+EgizianoEG@users.noreply.github.com>
2024-02-27 03:11:36 +0200

    modify event listener return type

--- Source/Typings/Libraries.d.ts
+++ Source/Typings/Libraries.d.ts
@@ -11,6 +11,18 @@ export declare module "utility-types" {
 }
 
 export declare module "discord.js" {
+  export interface InteractionCollector {
+    public on(event: string, listener: (...args: any[]) => Awaitable<any>): this;
+    public on(
+      event: 'end',
+      listener: (collected: Collection<Snowflake, Interaction>, reason: string) => Awaitable<any>,
+    ): this;
+    public on(
+      event: "collect" | "dispose" | "ignore",
+      listener: (interaction: Interaction) => Awaitable<any>
+    ): this;
+  }
+
   export interface Client {
     commands: Collection<string, SlashCommandObject>;
     cooldowns: Collection<string, Collection<string, number>>;
