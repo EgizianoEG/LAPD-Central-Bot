d6d73baa568c5ec63ec8bc81b9be4c92d4dac082
Egiziano <56704274+EgizianoEG@users.noreply.github.com>
2024-03-03 13:17:51 +0200

    optional expiration time

--- Source/Utilities/Other/ImgBBUpload.ts
+++ Source/Utilities/Other/ImgBBUpload.ts
@@ -6,9 +6,20 @@ import Axios from "axios";
  * Uploads an image to ImgBB.
  * @param Img - The image buffer to upload.
  * @param ImgName - Optional name for the image.
+ * @param Expiration - Optional expiration time for the image in seconds.
+ * Value must be between 60 and 15552000 seconds as specified by ImgBB.
+ *
  * @returns The direct URL of the uploaded image, or `null` if the upload fails.
  */
-export default async function UploadToImgBB(Img: Buffer, ImgName?: string): Promise<string | null> {
+export default async function UploadToImgBB(
+  Img: Buffer,
+  ImgName?: string,
+  Expiration?: number
+): Promise<string | null> {
+  if (Expiration && (Expiration < 60 || Expiration > 15_552_000)) {
+    throw new RangeError("Expiration must be between 60 and 15,552,000 seconds.");
+  }
+
   const Payload = new FormData();
   Payload.append("image", Img.toString("base64"));
 
@@ -16,6 +27,7 @@ export default async function UploadToImgBB(Img: Buffer, ImgName?: string): Prom
     params: {
       key: Other.ImgBB_API_Key,
       name: ImgName,
+      expiration: Expiration,
     },
   })
     .then((Resp) => Resp.data?.data.display_url || null)
