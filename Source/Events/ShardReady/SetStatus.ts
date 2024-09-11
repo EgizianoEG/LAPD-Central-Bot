a679d3a7db4ac08ef4ce1f0150706dff1b68cb61
Egiziano <56704274+EgizianoEG@users.noreply.github.com>
2024-09-11 07:03:19 +0300

    update status on shard ready

--- Source/Events/ShardReady/SetStatus.ts
+++ Source/Events/ShardReady/SetStatus.ts
@@ -1,15 +1,17 @@
 import { Client, ActivityType, PresenceUpdateStatus } from "discord.js";
 import AppLogger from "@Utilities/Classes/AppLogger.js";
 
-export default function SetAppStatus(Client: Client<true>) {
+export default function SetAppStatus(Client: Client<true>, ShardId: number) {
   Client.user.setStatus(PresenceUpdateStatus.Online);
   Client.user.setActivity({
     type: ActivityType.Watching,
     name: "and Dispatching",
+    shardId: ShardId,
   });
 
   AppLogger.log("info", {
-    label: "Events:Ready:SetStatus",
-    message: "Successfully set and updated the bot status.",
+    label: "Events:ShardReady:SetStatus",
+    message: "Successfully set and updated the bot status for the shard with id: %o.",
+    splat: [ShardId],
   });
 }
