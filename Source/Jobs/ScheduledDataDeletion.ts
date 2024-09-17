8b38380f059c0d22ccbc8ecff7d0c963751e3964
Egiziano <56704274+EgizianoEG@users.noreply.github.com>
2024-09-17 22:45:47 +0300

    delete associated guild data on guild document permenant deletion

--- Source/Jobs/ScheduledDataDeletion.ts
+++ Source/Jobs/ScheduledDataDeletion.ts
@@ -1,25 +1,32 @@
 import { CronJobFileDefReturn } from "@Typings/Global.js";
-import AppLogger from "@Utilities/Classes/AppLogger.js";
+import DeleteAllAssociatedGuildData from "@Utilities/Database/DeleteAssociatedGuildData.js";
 import GuildModel from "@Models/Guild.js";
+import AppLogger from "@Utilities/Classes/AppLogger.js";
 
 async function CleanupUnavailableGuilds(Now: Date | "init" | "manual") {
   const CurrentDate = Now instanceof Date ? Now : new Date();
-  const ScheduledGuildCount = await GuildModel.countDocuments({
-    deletion_scheduled_on: { $lte: CurrentDate },
-  }).exec();
+  const ScheduledGuilds = await GuildModel.find(
+    {
+      deletion_scheduled_on: { $lte: CurrentDate },
+    },
+    { deletion_scheduled_on: 1, _id: 1 },
+    { lean: true }
+  ).exec();
 
-  if (ScheduledGuildCount === 0) return;
+  if (ScheduledGuilds.length === 0) return;
   return GuildModel.deleteMany({
     deletion_scheduled_on: { $lte: CurrentDate },
   })
     .exec()
     .then((DeleteResult) => {
-      AppLogger.info({
-        splat: [DeleteResult.deletedCount, ScheduledGuildCount],
+      AppLogger.debug({
+        splat: [DeleteResult.deletedCount, ScheduledGuilds.length],
         label: "Jobs:ScheduledDataDeletion",
         message:
-          "%i out of %i guilds was successfully deleted from the database due to their deletion schedule.",
+          "%i out of %i guilds was successfully deleted from the database due to their deletion schedule. Continuing to delete associated profiles and data...",
       });
+
+      return DeleteAllAssociatedGuildData(ScheduledGuilds.map((Guild) => Guild._id));
     });
 }
 
