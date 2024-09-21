312405408de25fe234147dd96bda661191a7c601
Egiziano <56704274+EgizianoEG@users.noreply.github.com>
2024-09-21 20:46:19 +0300

    feat: add two new development commands ('/filter-input' and '/automod-rules-show')

---
+++ Source/Commands/Development/FilterInputString.ts
@@ -0,0 +1,70 @@
+import { FilterUserInput } from "@Utilities/Strings/Redactor.js";
+import {
+  SlashCommandBuilder,
+  InteractionContextType,
+  SlashCommandSubcommandsOnlyBuilder,
+  ActionRowBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  EmbedBuilder,
+  ModalBuilder,
+  codeBlock,
+  Colors,
+} from "discord.js";
+
+// ---------------------------------------------------------------------------------------
+/**
+ * @param Client
+ * @param Interaction
+ */
+async function Callback(Interaction: SlashCommandInteraction<"cached">) {
+  const InputModal = new ModalBuilder()
+    .setCustomId(`filter-input:${Interaction.user.id}:${Interaction.createdTimestamp}`)
+    .setTitle("Filter Input")
+    .setComponents(
+      new ActionRowBuilder<TextInputBuilder>().setComponents(
+        new TextInputBuilder()
+          .setCustomId("input")
+          .setLabel("Input String")
+          .setStyle(TextInputStyle.Paragraph)
+          .setRequired(true)
+      )
+    );
+
+  await Interaction.showModal(InputModal);
+  const ModalSubmission = await Interaction.awaitModalSubmit({
+    filter: (MS) => MS.customId === InputModal.data.custom_id,
+    time: 10 * 60 * 1000,
+  }).catch(() => null);
+
+  if (!ModalSubmission) return;
+  await ModalSubmission.deferReply({ ephemeral: true });
+
+  const InputString = ModalSubmission.fields.getTextInputValue("input");
+  const FilteredString = await FilterUserInput(InputString, {
+    guild_instance: Interaction.guild,
+    filter_links_emails: true,
+    replacement: "#",
+  });
+
+  const ResponseMessage = new EmbedBuilder()
+    .setColor(Colors.DarkAqua)
+    .setDescription(`**Here is the filtered input:**\n${codeBlock(FilteredString)}`);
+
+  return ModalSubmission.editReply({ embeds: [ResponseMessage] });
+}
+
+// ---------------------------------------------------------------------------------------
+// Command structure:
+// ------------------
+const CommandObject: SlashCommandObject<SlashCommandSubcommandsOnlyBuilder> = {
+  callback: Callback,
+  options: { dev_only: true },
+  data: new SlashCommandBuilder()
+    .setName("filter-input")
+    .setContexts(InteractionContextType.Guild)
+    .setDescription("Filters an input string."),
+};
+
+// ---------------------------------------------------------------------------------------
+export default CommandObject;
---
+++ Source/Commands/Development/ShowAutomodRules.ts
@@ -0,0 +1,72 @@
+import Dedent from "dedent";
+import HandleEmbedPagination from "@Utilities/Other/HandleEmbedPagination.js";
+import { ErrorEmbed } from "@Utilities/Classes/ExtraEmbeds.js";
+import {
+  EmbedBuilder,
+  SlashCommandBuilder,
+  InteractionContextType,
+  SlashCommandSubcommandsOnlyBuilder,
+} from "discord.js";
+
+// ---------------------------------------------------------------------------------------
+/**
+ * @param Client
+ * @param Interaction
+ */
+async function Callback(Interaction: SlashCommandInteraction<"cached">) {
+  const AutomoderationRules = await Interaction.guild.autoModerationRules.fetch();
+
+  if (AutomoderationRules.size === 0) {
+    return new ErrorEmbed()
+      .setDescription("There are no auto-moderation rules in this guild to show.")
+      .replyToInteract(Interaction, true);
+  }
+
+  const JSONConverted = AutomoderationRules.toJSON();
+  let JsonString = JSON.stringify(JSONConverted, null, 2);
+  const MaxLength = 1994;
+  const Pages: EmbedBuilder[] = [];
+
+  while (JsonString.length > MaxLength) {
+    const PageContent = JsonString.substring(0, MaxLength);
+    Pages.push(
+      new EmbedBuilder().setDescription(
+        Dedent(`
+          \`\`\`json
+          ${PageContent}
+          \`\`\`
+        `)
+      )
+    );
+    JsonString = JsonString.substring(MaxLength);
+  }
+
+  if (JsonString.length > 0) {
+    Pages.push(
+      new EmbedBuilder().setDescription(
+        Dedent(`
+          \`\`\`json
+          ${JsonString}
+          \`\`\`
+        `)
+      )
+    );
+  }
+
+  return HandleEmbedPagination(Pages, Interaction, undefined, true);
+}
+
+// ---------------------------------------------------------------------------------------
+// Command structure:
+// ------------------
+const CommandObject: SlashCommandObject<SlashCommandSubcommandsOnlyBuilder> = {
+  callback: Callback,
+  options: { dev_only: true },
+  data: new SlashCommandBuilder()
+    .setName("automod-rules-show")
+    .setContexts(InteractionContextType.Guild)
+    .setDescription("Shows auto-moderation rules for this server as a JSON object."),
+};
+
+// ---------------------------------------------------------------------------------------
+export default CommandObject;
