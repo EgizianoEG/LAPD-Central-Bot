915da10b0409a8a667d19d1ea23231de8346a1b1
Egiziano <56704274+EgizianoEG@users.noreply.github.com>
2024-09-24 10:18:39 +0300

    refactor: remove unused types for groupBy() methods as they are now in typescript base typings

--- Source/Typings/Libraries.d.ts
+++ Source/Typings/Libraries.d.ts
@@ -45,34 +45,4 @@ export declare module "discord.js" {
   }
 }
 
-declare global {
-  // A workaround until type definitions for Object/Map.groupBy are included in compiling.
-  interface MapConstructor {
-    groupBy<Item, Key>(
-      items: Iterable<Item>,
-      keySelector: (item: Item, index: number) => Key
-    ): Map<Key, Item[]>;
-  }
-
-  interface Object {
-    /**
-     * Returns an object that groups the iterable of the iterable object into arrays, using the return value of the callback function as the key.
-     * @param iterable An iterable object
-     * @param callbackfn A function that accepts up to two arguments. The map method calls the callbackfn function one time for each element in `iterable`.
-     */
-    groupBy<T, K extends PropertyKey>(
-      iterable: Iterable<T>,
-      callbackfn: (value: T, index: number) => K
-    ): Record<K, T[]>;
-
-    /**
-     * Returns an object that groups the iterable of the iterable object into arrays, using the return value of the callback function as the key.
-     * @param iterable An iterable object
-     * @param callbackfn A function that accepts up to two arguments. The map method calls the callbackfn function one time for each element in `iterable`.
-     */
-    groupBy(
-      iterable: Iterable<any>,
-      callbackfn: (value: any, index: number) => PropertyKey
-    ): Record<PropertyKey, any[]>;
-  }
-}
+declare global {}
