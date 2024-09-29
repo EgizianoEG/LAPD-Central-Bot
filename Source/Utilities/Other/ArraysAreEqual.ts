04c30305ca6c30bd237dbba482a7434c36ed52dc
Egiziano <56704274+EgizianoEG@users.noreply.github.com>
2024-09-29 17:30:37 +0300

    minor clarification

--- Source/Utilities/Other/ArraysAreEqual.ts
+++ Source/Utilities/Other/ArraysAreEqual.ts
@@ -1,8 +1,8 @@
 /**
  * Checks if two arrays are equal by comparing their lengths and sorted string representations.
- * @param Arr_1 - An array of any type, denoted as Arr_1.
+ * @param Arr_1 - An array of any type, denoted as `Arr_1`.
  * @param Arr_2 - An array of any type.
- * @returns a boolean value, indicating whether the two input arrays are equal or not.
+ * @returns a boolean value, indicating whether the two input arrays are equal or not, ***disregarding their element order***.
  */
 export function ArraysAreEqual(Arr_1: Array<any>, Arr_2: Array<any>): boolean {
   if (Arr_1 === Arr_2) return true;
