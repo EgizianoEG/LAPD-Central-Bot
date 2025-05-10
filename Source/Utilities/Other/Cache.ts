027da54fb06647194d7a8627725fbc380d2d0ef9
Egiziano <56704274+EgizianoEG@users.noreply.github.com>
2025-05-10 18:19:23 +0300

    refactor(cache): standardize cache configurations and improve TTL and checkperiod values for better performance and memory handling

--- Source/Utilities/Other/Cache.ts
+++ Source/Utilities/Other/Cache.ts
@@ -1,86 +1,82 @@
 import { secondsInDay } from "date-fns/constants";
 import NodeCache from "node-cache";
 
+const DefaultCheckPeriod = 5 * 60;
 export const RobloxAPICache = {
-  UsernameSearches: new NodeCache({ stdTTL: 5 * 60, useClones: false }),
-  IdByUsername: new NodeCache({ stdTTL: 5 * 60, useClones: false }),
+  QueryUsernameResultsCache: new NodeCache({
+    stdTTL: 60,
+    useClones: false,
+    checkperiod: DefaultCheckPeriod,
+  }),
+
+  IdByUsername: new NodeCache({
+    stdTTL: 5 * 60,
+    useClones: false,
+    checkperiod: DefaultCheckPeriod,
+  }),
 };
 
 export const DBRolePermsCache = new NodeCache({
-  stdTTL: 0.5 * 60,
+  stdTTL: 20,
   useClones: false,
-  deleteOnExpire: true,
+  checkperiod: DefaultCheckPeriod,
 });
 
 export const UserPermsCache = new NodeCache({
-  stdTTL: 0.5 * 60,
+  stdTTL: 20,
   useClones: false,
-  deleteOnExpire: true,
+  checkperiod: DefaultCheckPeriod,
 });
 
 export const BloxlinkDiscordToRobloxUsageChache = new NodeCache({
   stdTTL: secondsInDay,
   useClones: false,
-  deleteOnExpire: true,
 });
 
 export const IncidentAutocompletionCache = new NodeCache({
-  stdTTL: 5,
-  checkperiod: 5,
+  stdTTL: 15,
   useClones: false,
-  deleteOnExpire: true,
+  checkperiod: DefaultCheckPeriod,
 });
 
 export const CitationAutocompletionCache = new NodeCache({
-  stdTTL: 10,
-  checkperiod: 5,
+  stdTTL: 15,
   useClones: false,
-  deleteOnExpire: true,
+  checkperiod: DefaultCheckPeriod,
 });
 
 export const BookingAutocompletionCache = new NodeCache({
-  stdTTL: 10,
+  stdTTL: 15,
   useClones: false,
-  checkperiod: 5,
-  deleteOnExpire: true,
-});
-
-export const RobloxQueryUsernameResultsCache = new NodeCache({
-  stdTTL: 30,
-  checkperiod: 10,
-  useClones: false,
-  deleteOnExpire: true,
+  checkperiod: DefaultCheckPeriod,
 });
 
 export const GuildSettingsCache = new NodeCache({
   stdTTL: 10,
-  checkperiod: 5,
   useClones: false,
-  deleteOnExpire: true,
+  checkperiod: DefaultCheckPeriod,
 });
 
 export const GuildMembersCache = new NodeCache({
   stdTTL: 45,
-  checkperiod: 30,
   useClones: false,
-  deleteOnExpire: true,
+  checkperiod: DefaultCheckPeriod,
 });
 
 export const OngoingServerMemberNicknamesReplaceCache = new NodeCache({
   stdTTL: 6 * 60 * 60,
-  checkperiod: 5 * 60,
   useClones: false,
-  deleteOnExpire: true,
+  checkperiod: DefaultCheckPeriod,
 });
 
 export const UserCommandExecutionsCache = new NodeCache({
   stdTTL: 3600,
   useClones: false,
-  deleteOnExpire: true,
+  checkperiod: DefaultCheckPeriod,
 });
 
 export const GuildCommandExecutionsCache = new NodeCache({
   stdTTL: 3600,
   useClones: false,
-  deleteOnExpire: true,
+  checkperiod: DefaultCheckPeriod,
 });
