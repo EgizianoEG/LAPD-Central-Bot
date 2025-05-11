569da9f9bb751954bc452e69df860f2a29d7ecb7
Egiziano <56704274+EgizianoEG@users.noreply.github.com>
2025-05-11 21:39:14 +0300

    refactor: update GetValidTargetShiftTypes to normalize shift type names

--- Source/Utilities/Other/GetTargetShiftType.ts
+++ Source/Utilities/Other/GetTargetShiftType.ts
@@ -5,6 +5,7 @@ import { ShiftTypeExists } from "@Utilities/Database/ShiftTypeValidators.js";
  * Retrieves the **valid** target shift type(s) based on the provided interaction options.
  * @param Interaction - The slash command interaction object.
  * @param ValidateExisting - A boolean flag indicating whether to validate the existence of the shift types in the settings. Defaults to `false`.
+ * @remarks Normalizes the shift type names to a standard format (e.g., "Default" for "default").
  * @returns A promise that resolves to a tuple containing two arrays:
  * - The first array contains the *valid* target shift types.
  * - The second array contains the target shift types.
@@ -13,16 +14,22 @@ export default async function GetValidTargetShiftTypes(
   Interaction: SlashCommandInteraction<"cached">,
   ValidateExisting: boolean = false
 ): Promise<[string[], string[]]> {
-  const CmdShiftType = Interaction.options.getString("type", false)?.trim();
   let TargetShiftTypes: string[] = [];
   let ValidShiftTypes: string[] = [];
+  const CmdShiftType =
+    Interaction.options.getString("type", false)?.trim() ??
+    Interaction.options.getString("shift-type", false)?.trim();
 
   // Shift type name must not be less than 3 characters, and as a result,
   // the minimum length for multiple shift types must be at least 7 characters long when comma separated.
   if (CmdShiftType && CmdShiftType.length >= 7 && CmdShiftType.includes(",")) {
     TargetShiftTypes = CmdShiftType.trim()
       .split(",")
-      .map((Type) => Type.trim());
+      .map((Type) => {
+        Type = Type.trim();
+        if (Type.toLowerCase() === "default") return "Default";
+        return Type;
+      });
 
     if (ValidateExisting) {
       ValidShiftTypes = await Promise.all(
